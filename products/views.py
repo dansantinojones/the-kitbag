f02eb7384a30a35552d8a38f7dfb5b7b2400a0d5
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-19 11:52:46 +0000

    Create view to display all shirts

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Shirt
 
 # Create your views here.
+
+
+def all_shirts(request):
+    """ A view to show all products, sorting and search queries """
+
+    shirts = Shirt.objects.all()
+
+    context = {
+        'shirts': shirts,
+    }
+
+    return render(request, 'products/shirts.html', context)
