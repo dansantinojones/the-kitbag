4f6fb06f6d479bda2361fb286edbf973ce65a36c
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-20 09:17:52 +0000

    Add shirt detail url path

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_shirts, name='shirts')
+    path('', views.all_shirts, name='shirts'),
+    path('<shirt_id>', views.shirt_detail, name='shirt_detail'),
 ]
