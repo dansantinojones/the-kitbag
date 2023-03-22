409853ba30f3f58fca112331cd0d29d583481478
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-22 18:00:21 +0000

    Add filtering for shoppers to choose shirts from spesific leagues

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages 
 from django.db.models import Q
-from .models import Shirt
+from .models import Shirt, League
 
 # Create your views here.
 
@@ -11,8 +11,14 @@ def all_shirts(request):
 
     shirts = Shirt.objects.all()
     query = None
+    leagues = None
 
     if request.GET:
+        if 'league' in request.GET:
+            leagues = request.GET['league'].split(',')
+            shirts = shirts.filter(league__name__in=leagues)
+            leagues = League.objects.filter(name__in=leagues)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +31,7 @@ def all_shirts(request):
     context = {
         'shirts': shirts,
         'search_term': query,
+        'current_leagues': leagues,
     }
 
     return render(request, 'shirts/shirts.html', context)
