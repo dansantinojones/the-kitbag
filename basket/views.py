753caf9e02eec1053cfa7ab9be296780e51de8fd
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-25 09:22:51 +0000

    Create add to basket view with update session functionality

--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def view_basket(request):
-    """ A view that renders the basket content oage """
+    """ A view that renders the basket content page """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the spesific shirt to the basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
