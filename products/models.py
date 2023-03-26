eb269fcbf8298a964dfd754c8d0024fda124b19a
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-26 14:17:38 +0000

    Add size field and remove image url from shirt model and migrate changes

--- products/models.py
+++ products/models.py
@@ -18,8 +18,8 @@ class Shirt(models.Model):
     sku = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
     description = models.TextField()
+    size = models.CharField(max_length=10, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
