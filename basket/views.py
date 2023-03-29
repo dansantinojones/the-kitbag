0a3d27f4fd32db8943d6385380ab835b5d89d30c
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-29 12:04:13 +0000

    Update add to basket view removing the option to select a size

--- basket/views.py
+++ basket/views.py
@@ -10,16 +10,15 @@ def view_basket(request):
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the spesific shirt to the basket """
+    """ Add a spesific shirt to the basket """
 
-    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+        basket[item_id] += 1
     else:
-        basket[item_id] = quantity
+        basket[item_id] = 1
 
     request.session['basket'] = basket
     return redirect(redirect_url)
