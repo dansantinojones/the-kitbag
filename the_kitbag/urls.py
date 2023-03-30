52bcb61847a23050c0580fe360fcf0f4a8dbc387
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-30 07:44:33 +0000

    Add checkout url path

--- the_kitbag/urls.py
+++ the_kitbag/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('shirts/', include('products.urls')),
     path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
