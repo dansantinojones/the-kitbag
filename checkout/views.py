2ce48c6359aee344c4651765a966ad4c2db3d60e
dansantinojones <dansantinojones@hotmail.co.uk>
2023-03-30 07:42:30 +0000

    Create checkout view rendering payment form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import PaymentForm
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "Your basket is empty!")
+        return redirect(reverse('shirts'))
+
+    payment_form = PaymentForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'payment_form': payment_form,
+    }
+
+    return render(request, template, context)
