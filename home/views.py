3aac83cf4bc49e3fcf3afb91189a0cd999d94fbf
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-02 09:36:56 +0000

    Add views to render about us, contact us, faqs and returns pages

--- home/views.py
+++ home/views.py
@@ -7,3 +7,27 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A view to return the about us page """
+
+    return render(request, 'home/about_us.html')
+
+
+def contact_us(request):
+    """ A view to return the contact us page """
+
+    return render(request, 'home/contact_us.html')
+
+
+def faqs(request):
+    """ A view to return the FAQ's page """
+
+    return render(request, 'home/faqs.html')
+
+
+def returns(request):
+    """ A view to return the returns policy page """
+
+    return render(request, 'home/returns.html')
