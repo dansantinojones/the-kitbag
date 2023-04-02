45f66d655cc16955956a4ebcb04f700b855feca9
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-02 09:36:21 +0000

    Add url path for about us, contact us, faqs and returns

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('returns/', views.returns, name='returns'),
 ]
