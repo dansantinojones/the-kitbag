e359bc611340a34e02604a2e76fccee551397096
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-02 14:26:08 +0000

    Update view with update user info form

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the users profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html',
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
 
