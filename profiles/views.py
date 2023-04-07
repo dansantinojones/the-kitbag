c97958c548266f251a2ef60d1c2a1dd56638cccc
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-07 22:20:34 +0000

    Fix import typo error

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.contib import messages
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
