e4b8e3881c71a6ff2b4150f3d035492fed6f4cb1
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-07 13:43:45 +0000

    Add sell shirt and add shirt url path

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_shirts, name='shirts'),
-    path('<shirt_id>', views.shirt_detail, name='shirt_detail'),
+    path('<int:shirt_id>/', views.shirt_detail, name='shirt_detail'),
+    path('add/', views.add_shirt, name='add_shirt'),
+    path('sell_shirt/', views.sell_shirt, name='sell_shirt'),
 ]
