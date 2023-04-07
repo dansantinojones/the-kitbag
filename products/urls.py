6db6e25778a93b0dc7dd33dd1b08ec6dc1926b65
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-07 20:19:35 +0000

    Add edit shirt url path

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:shirt_id>/', views.shirt_detail, name='shirt_detail'),
     path('add/', views.add_shirt, name='add_shirt'),
     path('sell_shirt/', views.sell_shirt, name='sell_shirt'),
+    path('edit/<int:shirt_id>/', views.edit_shirt, name='edit_shirt'),
 ]
