fd83225106515edc640192d9acd6727160074643
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-08 11:25:33 +0000

    Add newsletter admin to manage emails in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Newsletter
 
 # Register your models here.
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'added_on',
+    )
+
+    ordering = ('added_on',)
+
+admin.site.register(Newsletter, NewsletterAdmin)
