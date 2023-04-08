327572bdde81ba55d5ef37af9b2dcff348b66d81
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-08 11:26:18 +0000

    Add newsletter model to record sign ups to add to mail list

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Newsletter(models.Model):
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
