2ec54040ae7dc8208a394820acc817c676d88f4d
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-10 11:27:49 +0000

    Create reviews admin to manage reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'created_on',
+        'rating',
+        'body',
+    )
+
+    ordering = ('created_on',)
+
+admin.site.register(Review, ReviewsAdmin)
