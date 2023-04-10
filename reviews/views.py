cc222bd49236941c89bc5d4199e841c9ece41a18
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-10 08:45:01 +0000

    Add view to return the reviews page

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def reviews(request):
+    """ A view to return the reviews page """
+
+    return render(request, 'reviews/reviews.html')
