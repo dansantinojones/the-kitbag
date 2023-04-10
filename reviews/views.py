0dde79bd5ab3cae8a3d05015bcd200563298cd83
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-10 10:16:55 +0000

    Add post review view to handle form submission

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Review
+from .forms import PostReviewForm
 
 # Create your views here.
 
@@ -6,3 +10,23 @@ def reviews(request):
     """ A view to return the reviews page """
 
     return render(request, 'reviews/reviews.html')
+
+
+@login_required
+def post_review(request):
+    """ Post a review """
+    if request.method == 'POST':
+        form = PostReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thanks for posting a review!')
+            return redirect(reverse('reviews'))
+        else:
+            form = PostReviewForm()
+
+    form = PostReviewForm()
+    template = 'reviews/reviews.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
