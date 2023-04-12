d0b53d7734dd250e05455ac99a9e61b940ead443
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-12 16:49:47 +0000

    Fix return error by changing name to full name

--- reviews/models.py
+++ reviews/models.py
@@ -14,4 +14,4 @@ class Review(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.full_name}"
