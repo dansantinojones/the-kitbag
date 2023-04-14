94b512f57bee48ce9b3956b3a5712a31c3d14303
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-14 13:43:46 +0000

    Fix typo in cache control

--- the_kitbag/settings.py
+++ the_kitbag/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     # Cache Control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheConrol': 'max-age=94608000',
+        'CacheControl': 'max-age=94608000',
     }
 
     # Bucket Config
