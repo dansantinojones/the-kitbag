75724af5b9ffa4e7b6f0ee1f3fabc36045bf511f
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-14 07:22:34 +0000

    Add cache control for deployment

--- the_kitbag/settings.py
+++ the_kitbag/settings.py
@@ -189,6 +189,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
+        'CacheConrol': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'the-kitbag'
     AWS_S3_REGION_NAME = 'us-east-1'
