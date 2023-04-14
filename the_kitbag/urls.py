b9437fc22f3989f0adf58e23ae8a3988673309f5
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-14 15:11:14 +0000

    Create 404 error template, view and url

--- the_kitbag/urls.py
+++ the_kitbag/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'the_kitbag.views.handler404'
