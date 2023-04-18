b9e6ffd2dd6fcd199175f62fd8ac23f5df6bb357
dansantinojones <dansantinojones@hotmail.co.uk>
2023-04-18 18:31:38 +0000

    Add email port to send real emails

--- the_kitbag/settings.py
+++ the_kitbag/settings.py
@@ -225,6 +225,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
